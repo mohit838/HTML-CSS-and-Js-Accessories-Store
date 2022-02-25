4e2e53d65e034d648e207dbcccbbe0a0c403041e
mohit838 <mohitul449@gmail.com>
2022-02-25 16:08:15 +0600

    preloader

---
+++ Preloader/active.js
@@ -0,0 +1,7 @@
+//Preloader Js
+/* Main Preloader Code {Copy Code}*/
+let preLoader = document.getElementById('preloader');
+
+window.addEventListener('load',()=>{
+    preLoader.style.display = 'none';
+})
---
+++ README.md
@@ -0,0 +1,8 @@
+## HTML-CSS-and-Js-Accessories-Store
+<b>All kinds of Accessories Store</b>
+<ul>
+<li>Preloader</li>
+<li>Navbar</li>
+</ul>
+
+>> Hava Fun with this Accessories-Store
